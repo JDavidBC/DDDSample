0cfe732f69cf9626ad72d9e6aafa0117224b7b1e
Jesus David <jesus.david@me.com>
2020-02-11 11:24:03 +0100

    changes

---
+++ DomainCore/Source.DataServices.Interfaces/ICaregiversDataService.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using Source.Core.DataService;
+using System.Threading.Tasks;
+using Source.Domains.Entities;
+
+namespace Source.DataServices.Interfaces
+{
+    public interface ICaregiversDataService: IEntityDataService<Caregivers>
+    {
+        Task<IList<Caregivers>> GetByFirstName(string firstName);
+
+    }
+}
